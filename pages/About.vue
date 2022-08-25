d718ccb32ddcef28e33dbbc6d27ccef4d56fdb94
holgermueller <9jholger@gmail.com>
2022-08-24 21:30:57 -0500

    added some content to about page

--- pages/About.vue
+++ pages/About.vue
@@ -5,7 +5,33 @@
         <h1 class="display-4">About Me</h1>
       </v-card-title>
       <v-card-text>
-        <p></p>
+        <p class="about-paragraph">
+          I'm a full-stack front-end developer living in Batesville, Arkansas.
+        </p>
+        <p class="about-paragraph">
+          Originally from Upstate New York, I moved to Batesville, Arkansas
+          halfway through 2022 so my wife could pursue her dream job, working at
+          a small liberal arts college. Prior to Arkansas, I lived in
+          Minneapolis, MN, where I endured the winters for seven years. During
+          that time, I earned my full-stack web developer certificate through
+          the University of Minnesota's College of Continuing Education. Through
+          that program, I've become proficient in Javascript, jQuery, React,
+          VueJs, Google's Cloud Firebase, MongoDB, and a few other technologies.
+          My plan of attack on projects is to break the overall task into the
+          smallest manageable pieces and build up from there.
+        </p>
+        <p class="about-paragraph">
+          When not coding, I enjoy reading and writing, watching movies, hiking,
+          and travelling. One of my longshot dreams is to visit every country on
+          the planet. Some of my bucket-list destinations are Japan, New
+          Zealand, Portugal, South Africa, and Iceland. So far, I've been to
+          Australia, the Republic of Korea, France, Canada, and Italy. As far as
+          reading goes, I've recently rediscovered my love of Science Ficiton
+          and fantasy, and I've always had an affinty for Marvel Comics. With
+          movies, I tend to enjoy comedies over other genres. And with music,
+          I'm fond of many genres, but jazz and punk rock tend to be my
+          favorites.
+        </p>
       </v-card-text>
     </v-card>
   </div>
@@ -24,5 +50,11 @@ export default {
 }
 h1 {
   letter-spacing: 4px;
+}
+.about-paragraph {
+  text-align: justify;
+  text-justify: inter-word;
+  margin-left: 10%;
+  margin-right: 10%;
 }
 </style>
