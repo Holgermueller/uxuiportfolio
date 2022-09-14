2cc27ba25a3983cf90fed572e86ff3155b504445
holgermueller <9jholger@gmail.com>
2022-09-13 21:37:37 -0500

    created dynamic cards for portfoliopage

--- pages/Portfolio.vue
+++ pages/Portfolio.vue
@@ -1,62 +1,74 @@
 <template>
   <div id="Portfolio">
-    <v-hover v-slot="{ hover }">
-      <v-card>
-        <v-expand-transition>
-          <div
-            v-if="hover"
-            class="d-flex transition-fast-in-fast-out primary darken-2 v-card--reveal text-h2 white--text"
-            style="height: 100%"
-          >
-            Demo transition
-          </div>
-        </v-expand-transition>
-        <v-card-text>
-          <h1>Demo Card</h1>
-        </v-card-text>
-      </v-card>
-      <!-- <v-card class="portfolio-card" elevation="0">
-        <v-expand-x-transition>
-          <div
-            v-if="hover"
-            class="d-flex transition-fast-in-fast-out primary"
-            style="height: 100%"
-          >
-            <v-card-title>
-              <v-img class="img-even" src="./fts.png">
-                <h1 class="display-4 primary--text">Portfolio</h1></v-img
-              >
-            </v-card-title>
-            <v-card-text>
-              <div class="project-display">
-                <div class="project-text-even">
-                  <h2 class="project-title">Fearless Ticket Sellers</h2>
-                  <p>
-                    How do I purchase tickets for smaller, unorthodox venues?
-                  </p>
-                  <nuxt-link to="/FTSCaseStudy">
-                    <p class="title project-link">
-                      See case study <v-icon right>mdi-arrow-right</v-icon>
-                    </p>
-                  </nuxt-link>
+    <v-card class="portfolio-card" elevation="0">
+      <v-card-title>
+        <h1 class="display-4 primary--text">Portfolio</h1>
+      </v-card-title>
+    </v-card>
+
+    <v-container class="portfolio" fluid>
+      <v-layout row wrap>
+        <v-flex
+          xs12
+          sm12
+          md6
+          lg6
+          xl6
+          v-for="(project, index) in projects"
+          :key="index"
+        >
+          <v-hover v-slot="{ hover }">
+            <v-card class="project-card">
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out primary darken-2 v-card--reveal display-4 white--text"
+                  style="height: 100%"
+                >
+                  <h2 class="title project-link text--white">
+                    <nuxt-link :to="project.link">
+                      {{ project.title }}
+                    </nuxt-link>
+                  </h2>
                 </div>
-              </div>
-            </v-card-text>
-          </div>
-        </v-expand-x-transition>
-      </v-card> -->
-    </v-hover>
+              </v-expand-transition>
+              <v-card-title> Title </v-card-title>
+              <v-card-text>
+                <h1>Demo Card</h1>
+              </v-card-text>
+            </v-card>
+          </v-hover>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'PortfolioPage',
+
+  data: () => ({
+    projects: [
+      { title: 'Jobz', link: '/JobzCasestudy' },
+      { title: 'Hepcats', link: '/HepcatsCasestudy' },
+      { title: 'Fearless Ticket Sellers', link: '/FTSCaseStudy' },
+    ],
+  }),
+}
+</script>
 
 <style scoped>
 .portfolio-card {
   width: 85%;
   margin: auto;
 }
-
 h1 {
   letter-spacing: 4px;
+}
+
+.project-card {
+  margin: 8px;
 }
 
 .v-card--reveal {
@@ -74,6 +86,7 @@ h1 {
 
 a {
   text-decoration: none;
+  color: white;
 }
 @media (min-width: 360px) and (max-width: 450px) {
   .v-application .display-4 {
