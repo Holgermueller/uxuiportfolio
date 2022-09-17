45dea0fb14a2aa65336a62de131acfeeab389d93
holgermueller <9jholger@gmail.com>
2022-09-17 11:35:53 -0500

    added socials to home page

--- pages/index.vue
+++ pages/index.vue
@@ -22,12 +22,45 @@
         </h3>
       </v-card-text>
     </v-card>
+
+    <div class="socials">
+      <div class="link" v-for="(link, i) in links" :key="i">
+        <a :href="link.link" target="_blank">
+          <v-icon x-large>{{ link.icon }} </v-icon>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+
+  data: () => ({
+    links: [
+      {
+        link: 'https://github.com/Holgermueller',
+        icon: 'mdi mdi-github',
+      },
+      {
+        link: 'https://stackoverflow.com/users/9111512/holger-mueller?tab=profile',
+        icon: 'mdi mdi-stack-overflow',
+      },
+      {
+        link: 'https://www.linkedin.com/in/holger-mueller-75855114a/',
+        icon: 'mdi mdi-linkedin',
+      },
+      {
+        link: 'https://holgermueller.github.io/Resume/',
+        icon: 'mdi mdi-file',
+      },
+      {
+        link: 'mailto:9jholger@gmail.com',
+        icon: 'mdi mdi-email',
+      },
+    ],
+  }),
 }
 </script>
 
@@ -39,6 +72,28 @@ export default {
 h1 {
   letter-spacing: 4px;
 }
+.socials {
+  width: 55%;
+  margin: 2rem auto;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  column-gap: 0.5rem;
+  flex-wrap: wrap;
+}
+a {
+  text-decoration: none;
+}
+.link,
+.v-icon {
+  border-radius: 5px;
+}
+.v-icon:hover,
+.link:hover {
+  background-color: #13a7be;
+  color: white;
+}
+
 @media (min-width: 360px) and (max-width: 450px) {
   .v-application .display-4 {
     font-size: 2.5rem !important;
